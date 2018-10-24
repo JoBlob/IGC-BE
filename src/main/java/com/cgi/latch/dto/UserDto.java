002e3f16595a862bb9c5e910506039bd919dc800
fabio.fonseca <fabio.fonseca@CA-L2XFJKP2.groupinfra.com>
2018-10-24 16:51:37 -0400

    userDTO complete

--- src/main/java/com/cgi/latch/dto/UserDto.java
+++ src/main/java/com/cgi/latch/dto/UserDto.java
@@ -1,8 +1,6 @@
 package com.cgi.latch.dto;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
@@ -10,8 +8,11 @@ import javax.persistence.Table;
 public class UserDto {
 
     private String firstName;
+    private String lastName;  
 
-    private String lastName;
+	private int employeeId;
+    private String password;
+    private String email;
 
     public String getFirstName() {
         return firstName;
@@ -28,4 +29,28 @@ public class UserDto {
     public void setLastName(String lastnName) {
         this.lastName = lastnName;
     }
+    
+    public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }
