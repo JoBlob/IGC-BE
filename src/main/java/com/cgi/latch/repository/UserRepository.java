b8479e120939dfacf4791013a5984127a93cc4e0
fabio.fonseca <fabio.fonseca@CA-L2XFJKP2.groupinfra.com>
2018-10-24 17:25:12 -0400

    commentaire sur UserRepository

--- src/main/java/com/cgi/latch/repository/UserRepository.java
+++ src/main/java/com/cgi/latch/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.cgi.latch.repository;
 import com.cgi.latch.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+//TODO: implementer avec la librarie spring data:
+//voir https://docs.spring.io/spring-data/data-commons/docs/1.6.1.RELEASE/reference/html/repositories.html
+
 public interface UserRepository extends CrudRepository<User, Integer> { }
 
 
