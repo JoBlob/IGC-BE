200325d29f396ae9417cb76aee4b3d80a59032bb
fabio.fonseca <fabio.fonseca@CA-L2XFJKP2.groupinfra.com>
2018-10-24 18:52:13 -0400

    comment on main class

--- src/main/java/com/cgi/latch/LatchApplication.java
+++ src/main/java/com/cgi/latch/LatchApplication.java
@@ -1,9 +1,14 @@
 package com.cgi.latch;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+
+// The annotation (exclude = SecurityAutoConfiguration.class) avoids the need for basic authentication when making an api call
+// see https://stackoverflow.com/questions/37285016/what-is-username-and-password-when-starting-spring-boot-with-tomcat
+// and https://stackoverflow.com/questions/40228036/how-to-turn-off-spring-security-in-spring-boot-application
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class LatchApplication {
 
