a250999652bd8aec5a285137c76ab2afe4e2f7b3
fabio.fonseca <fabio.fonseca@CA-L2XFJKP2.groupinfra.com>
2018-10-24 17:41:21 -0400

    User Controller uses a dto

--- src/main/java/com/cgi/latch/controller/UserController.java
+++ src/main/java/com/cgi/latch/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cgi.latch.dto.UserDto;
 import com.cgi.latch.model.User;
 
 @RestController
@@ -24,8 +25,8 @@ public class UserController {
 	
 	@RequestMapping(method=RequestMethod.POST)
 	@ResponseStatus(HttpStatus.CREATED)
-	public User createUser(@RequestBody User user) {
+	public UserDto createUser(@RequestBody UserDto userDto) {
 		//TODO: Convertir le DTO a un DAO et creer l'utilisateur dans la base de donnees
-		return user;
+		return userDto;
 	}
 }
